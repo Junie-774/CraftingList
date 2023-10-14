1284eb842109ade823b5673d6552db760f39bebf
drejjmit <drimit_cs@hotmail.com>
2023-10-14 12:24:11 +0200

    Update PtrRepair.cs

--- CraftingList/SeFunctions/PtrRepair.cs
+++ CraftingList/SeFunctions/PtrRepair.cs
@@ -1,16 +1,27 @@
-﻿using CraftingList.Utility;
+using CraftingList.Utility;
+using FFXIVClientStructs.Attributes;
 using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+[Addon("Repair")]
+[StructLayout(LayoutKind.Explicit, Size = 0xF7E8)]
+unsafe struct AddonRepair65
+{
+    [FieldOffset(616)] public AtkComponentButton* RepairAllButton;
+}
 
 namespace CraftingList.SeFunctions
 {
     public unsafe struct PtrRepair
     {
         private const int repairAllButtonId = 25;
-        AddonRepair* AddonPointer;
+        AddonRepair65* AddonPointer;
 
         public static implicit operator PtrRepair(IntPtr pointer)
-            => new() { AddonPointer = Module.Cast<AddonRepair>(pointer) };
+            => new() { AddonPointer = Module.Cast<AddonRepair65>(pointer) };
 
         public static implicit operator bool(PtrRepair addonRepair)
         {
